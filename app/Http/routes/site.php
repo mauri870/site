af32b7b84337189f2cfd5a270101b4ac98e370a7
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-01 00:32:17 -0200

    Added middleware for pointing the domain site to default locale

--- app/Http/routes/site.php
+++ app/Http/routes/site.php
@@ -1,5 +1,8 @@
 <?php
 // Site Routes
-Route::group(['prefix' => LaravelLocalization::setLocale()], function() {
+Route::group([
+    'prefix' => LaravelLocalization::setLocale(),
+    'middleware' => [ 'localeSessionRedirect', 'localizationRedirect']
+], function() {
     Route::get('/', ['as' => 'home.index', 'uses' => 'HomeController@index']);
 });
