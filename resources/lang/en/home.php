bd5e4b940572e39904c9b898936ee613094daecc
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-01 16:58:09 -0200

    Updated lang files

--- resources/lang/en/home.php
+++ resources/lang/en/home.php
@@ -3,5 +3,9 @@ return [
     'title' => 'Title',
     'description' => 'Description',
     'keywords' => 'keywords,test,hi',
-    'init_phrase' => 'PHP/Laravel developer from Brazil'
+    'init_phrase' => 'PHP/Laravel developer from Brazil',
+
+    //About me
+    'my_story'=>'About me',
+    'resume' => "Welcome! My name is Mauri and I am Php developer for 2 years. I work developing WebSites and web systems focusing on Mobile First using PHP framework Laravel and for smaller projects / micro-api's the Lumen . I am currently responsible for all areas from database to the configuration of VPS servers , so let's say my sweat is literally in every corner of projects. I love security and vulnerability analysis , web , technology and someday even create something innovative! Be sure to check my schedule and technology blog ;)",
 ];
