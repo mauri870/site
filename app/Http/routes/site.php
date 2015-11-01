309364885c6e8d0234d45ac9a396064274d5e17a
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-01 00:32:58 -0200

    Comment

--- app/Http/routes/site.php
+++ app/Http/routes/site.php
@@ -4,5 +4,6 @@ Route::group([
     'prefix' => LaravelLocalization::setLocale(),
     'middleware' => [ 'localeSessionRedirect', 'localizationRedirect']
 ], function() {
+    //Localized routes
     Route::get('/', ['as' => 'home.index', 'uses' => 'HomeController@index']);
 });
