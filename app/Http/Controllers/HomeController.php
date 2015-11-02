6a454226df1e9c9c1ecffddfebd6fc639ffa5ecd
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-01 23:43:22 -0200

    Github info is now cached for improve performance

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers;
 
 use Carbon\Carbon;
+use GrahamCampbell\GitHub\Facades\GitHub;
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Cache;
 
 class HomeController extends Controller
 {
@@ -19,6 +21,18 @@ class HomeController extends Controller
     {
         $my_age = Carbon::now()->diffInYears(Carbon::parse('1995-10-19 17:30:00'));
         $dev_time = Carbon::now()->diffInYears(Carbon::parse('2013-9-2 19:00:00'));
-        return view('index',compact('my_age','dev_time'));
+
+        if(!Cache::has('public_repos')){
+            $public_repos = GitHub::me()->show()['public_repos'];
+            $commits = GitHub::repo()->statistics('mauri870','site')[0]['total'];
+
+            Cache::put('public_repos', $public_repos, 30);
+            Cache::put('commits', $commits, 30);
+        }
+        
+        $public_repos = Cache::get('public_repos');
+        $commits = Cache::get('commits');
+
+        return view('index',compact('my_age','dev_time','public_repos','commits'));
     }
 }
