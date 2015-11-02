80e54c6f6cb7e8cfb8bb0807aedd77eb7e5c7650
Mauri de Souza Nunes <mauri870@gmail.com>
2015-11-02 14:33:20 -0200

    Added php doc blocks for functions

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,11 +17,28 @@ class HomeController extends Controller
 
     }
 
+    /**
+     * Index page
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $my_age = Carbon::now()->diffInYears(Carbon::parse('1995-10-19 17:30:00'));
         $dev_time = Carbon::now()->diffInYears(Carbon::parse('2013-9-2 19:00:00'));
 
+        $this->getGithubInfo();
+
+        $public_repos = Cache::get('public_repos');
+        $commits = Cache::get('commits');
+
+        return view('index',compact('my_age','dev_time','public_repos','commits'));
+    }
+
+
+    /**
+     * Get profile basic info and cached info
+     */
+    public function getGithubInfo(){
         if(!Cache::has('public_repos')){
             $public_repos = GitHub::me()->show()['public_repos'];
             $commits = GitHub::repo()->statistics('mauri870','site')[0]['total'];
@@ -29,10 +46,5 @@ class HomeController extends Controller
             Cache::put('public_repos', $public_repos, 30);
             Cache::put('commits', $commits, 30);
         }
-        
-        $public_repos = Cache::get('public_repos');
-        $commits = Cache::get('commits');
-
-        return view('index',compact('my_age','dev_time','public_repos','commits'));
     }
 }
