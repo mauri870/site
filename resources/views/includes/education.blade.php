2cae7b7b617075d6da0b26a3392076eb256917f2
Mauri de Souza Nunes <mauri870@gmail.com>
2016-01-12 15:41:48 -0200

    Moved education section to single blade file for better organization

---
+++ resources/views/includes/education.blade.php
@@ -0,0 +1,160 @@
+<section id="education" class="root-sec grey lighten-5 padd-tb-120 education-wrap">
+    <div class="container">
+        <div class="row">
+            <div class="education-inner">
+                <div class="col-sm-12 col-md-10 card-box-wrap">
+                    <div class="row">
+                        <div class="clearfix section-head education-text">
+                            <div class="col-sm-12">
+                                <h2 class="title">{{ trans('menu.education') }}</h2>
+                                <p class="regular-text"></p>
+                            </div>
+                        </div>
+                        <div class="col-lg-12">
+                            <div class="overflow-hidden">
+                                <div class="row">
+                                    <div id="educationSlider" class="clearfix card-element-wrapper">
+
+                                        <!-- single education -->
+                                        <div class="col-sm-4 single-card-box wow fadeInUpSmall" data-wow-duration=".7s">
+                                            <div class="card">
+                                                <div class="card-image waves-effect waves-block waves-light">
+                                                    <h2 class="left-align card-title-top">2013</h2>
+                                                    <div class="card-img-wrap">
+                                                        <img class="activator" src="{{ asset('images/grad-cap.png') }}" alt="">
+                                                    </div>
+                                                </div>
+                                                <div class="card-content">
+                                                    <span class="card-title activator brand-text">{{ trans('home.educ_course_ti') }}<i class="mdi-navigation-more-vert right"></i></span>
+                                                    <p>Ftec</p>
+                                                </div>
+                                                <!-- Reveal content-->
+                                                <div class="card-reveal">
+                                                    <div class="rev-title-wrap">
+                                                        <span class="card-title activator brand-text">{{ trans('home.educ_course_ti') }}<i class="mdi-navigation-close right"></i></span>
+                                                        <p>Ftec</p>
+                                                    </div>
+                                                    <p class="rev-content">
+                                                        {{ trans('home.educ_course_desc_ti') }}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div> <!-- single education end -->
+
+                                        <!-- single education -->
+                                        <div class="col-sm-4 single-card-box wow fadeInUpSmall" data-wow-duration=".7s">
+                                            <div class="card">
+                                                <div class="card-image waves-effect waves-block waves-light">
+                                                    <h2 class="left-align card-title-top">2015</h2>
+                                                    <div class="card-img-wrap">
+                                                        <img class="activator" src="{{ asset('images/cert.png') }}" alt="">
+                                                    </div>
+                                                </div>
+                                                <div class="card-content">
+                                                    <span class="card-title activator brand-text">{{ trans('home.educ_course_laravel') }}<i class="mdi-navigation-more-vert right"></i></span>
+                                                    <p>School of Net Internet Sistemas e Tecnologia Ltda</p>
+                                                </div>
+                                                <!-- Reveal content-->
+                                                <div class="card-reveal">
+                                                    <div class="rev-title-wrap">
+                                                        <span class="card-title activator brand-text">{{ trans('home.educ_course_laravel') }}<i class="mdi-navigation-close right"></i></span>
+                                                        <p>School of Net Internet Sistemas e Tecnologia Ltda</p>
+                                                    </div>
+                                                    <p class="rev-content">
+                                                        {{ trans('home.educ_course_desc_laravel') }}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div> <!-- single education end -->
+
+                                        <!-- single education -->
+                                        <div class="col-sm-4 single-card-box wow fadeInUpSmall" data-wow-duration=".7s">
+                                            <div class="card">
+                                                <div class="card-image waves-effect waves-block waves-light">
+                                                    <h2 class="left-align card-title-top">2015</h2>
+                                                    <div class="card-img-wrap">
+                                                        <img class="activator" src="{{ asset('images/cert.png') }}" alt="">
+                                                    </div>
+                                                </div>
+                                                <div class="card-content">
+                                                    <span class="card-title activator brand-text">{{ trans('home.educ_course_aws') }}<i class="mdi-navigation-more-vert right"></i></span>
+                                                    <p>School of Net Internet Sistemas e Tecnologia Ltda</p>
+                                                </div>
+                                                <!-- Reveal content-->
+                                                <div class="card-reveal">
+                                                    <div class="rev-title-wrap">
+                                                        <span class="card-title activator brand-text">{{ trans('home.educ_course_aws') }}<i class="mdi-navigation-close right"></i></span>
+                                                        <p>School of Net Internet Sistemas e Tecnologia Ltda</p>
+                                                    </div>
+                                                    <p class="rev-content">
+                                                        {{ trans('home.educ_course_desc_aws') }}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div> <!-- single education end -->
+
+                                        <!-- single education -->
+                                        <div class="col-sm-4 single-card-box wow fadeInUpSmall" data-wow-duration=".7s">
+                                            <div class="card">
+                                                <div class="card-image waves-effect waves-block waves-light">
+                                                    <h2 class="left-align card-title-top">2015</h2>
+                                                    <div class="card-img-wrap">
+                                                        <img class="activator" src="{{ asset('images/cert.png') }}" alt="">
+                                                    </div>
+                                                </div>
+                                                <div class="card-content">
+                                                    <span class="card-title activator brand-text">{{ trans('home.educ_course_lumen') }}<i class="mdi-navigation-more-vert right"></i></span>
+                                                    <p>School of Net Internet Sistemas e Tecnologia Ltda</p>
+                                                </div>
+                                                <!-- Reveal content-->
+                                                <div class="card-reveal">
+                                                    <div class="rev-title-wrap">
+                                                        <span class="card-title activator brand-text">{{ trans('home.educ_course_lumen') }}<i class="mdi-navigation-close right"></i></span>
+                                                        <p>School of Net Internet Sistemas e Tecnologia Ltda</p>
+                                                    </div>
+                                                    <p class="rev-content">
+                                                        {{ trans('home.educ_course_desc_lumen') }}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div> <!-- single education end -->
+
+                                        <!-- single education -->
+                                        <div class="col-sm-4 single-card-box wow fadeInUpSmall" data-wow-duration=".7s">
+                                            <div class="card">
+                                                <div class="card-image waves-effect waves-block waves-light">
+                                                    <h2 class="left-align card-title-top">2015</h2>
+                                                    <div class="card-img-wrap">
+                                                        <img class="activator" src="{{ asset('images/cert.png') }}" alt="">
+                                                    </div>
+                                                </div>
+                                                <div class="card-content">
+                                                    <span class="card-title activator brand-text">{{ trans('home.educ_course_linux') }}<i class="mdi-navigation-more-vert right"></i></span>
+                                                    <p>School of Net Internet Sistemas e Tecnologia Ltda</p>
+                                                </div>
+                                                <!-- Reveal content-->
+                                                <div class="card-reveal">
+                                                    <div class="rev-title-wrap">
+                                                        <span class="card-title activator brand-text">{{ trans('home.educ_course_linux') }}<i class="mdi-navigation-close right"></i></span>
+                                                        <p>School of Net Internet Sistemas e Tecnologia Ltda</p>
+                                                    </div>
+                                                    <p class="rev-content">
+                                                        {{ trans('home.educ_course_desc_linux') }}
+                                                    </p>
+                                                </div>
+                                            </div>
+                                        </div> <!-- single education end -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="btn-wrapp edu-ctrl">
+                    <a class="btn-floating waves-effect waves-light btn-large brand-bg go go-left"><i class="mdi-navigation-chevron-left"></i></a>
+                    <a class="btn-floating waves-effect waves-light btn-large brand-bg go go-right"><i class="mdi-navigation-chevron-right"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
